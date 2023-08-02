febcc92cf1d0f6406f6695a3df5625fc1aad6637
Sheelam Aravind <102232179+AravindSheelam@users.noreply.github.com>
2023-08-02 20:27:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 // your code here
- document.getElementById("urlForm").addEventListener("submit", function(event) {
-      event.preventDefault(); // Prevent form submission to stop page refresh
-      updateURL();
-    });
-
-    function updateURL() {
-      const name = document.getElementById("name").value;
-      const year = document.getElementById("year").value;
-
-      const url = `https://localhost:8080/?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
-      document.getElementById("url").textContent = url;
-    }
+let h3=document.getElementById("url");
+let value1=h3.innerText;
+function addTheme(){
+	let add=document.getElementById("add");
+	let name=document.getElementById("name");
+	let year=document.getElementById("year")
+	let nameValue=name.value;
+	let yearValue=year.value;
+	if(nameValue!=="" && yearValue!=="")
+	{
+		add.innerText=value1+`?name=${nameValue}&year=${yearValue}`
+		
+	}
+	else if(nameValue==="")
+	{
+		add.innerText=value1+`?year=${yearValue}`
+	}
+	else if(yearValue==="")
+	{
+		add.innerText=value1+`?name=${nameValue}`
+	}
+	else if(yearValue==="" && nameValue==="")
+	{
+		add.innerText=value1;
+	}
+	
+}
+const submit=document.getElementById("button");
+submit.addEventListener("click",addTheme)
